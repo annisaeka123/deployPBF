23cbb340fccb4809ce3ed2e379bac32bdef254af
annisa eka <annisaekapuspitaaa@gmail.com>
2026-04-07 21:48:41 +0700

    Praktikum 2 (deployment ke vercel): Gunakan Environment Variable

--- my-app/src/pages/produk/[produk].tsx
+++ my-app/src/pages/produk/[produk].tsx
@@ -19,7 +19,7 @@ export default HalamanProduk
 
 //digunakan server-side rendering
 export async function getServerSideProps({ params }: { params: { produk: string } }) {
-  const res = await fetch(`http://localhost:3000/api/produk/${params?.produk}`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/produk/${params?.produk}`);
   const respone = await res.json();
   // console.log("Data produk yang diambil dari API:", respone);
   return {
